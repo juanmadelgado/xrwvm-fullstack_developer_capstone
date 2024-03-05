33f2b7fd12cd45cf99fa4b7b8a4bdb793d6ecc67
juanmadelgado <juanmadel200@gmail.com>
2024-03-05 16:21:43 -0500

    update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -87,7 +87,6 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
-
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if (dealer_id):

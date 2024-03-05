1c72b51a82c6760bea8401e84bcdee5542a375ae
juanmadelgado <juanmadel200@gmail.com>
2024-03-05 18:25:10 -0500

    update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -79,13 +79,14 @@ def registration(request):
         return JsonResponse(data)
 
 
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
-    if (state == "All"):
+    if(state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =\
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'https://juanmadel200-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
 CSRF_TRUSTED_ORIGINS = ['https://juanmadel200-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
